e998e0fa4bddade0e86d79cc559dc28691a777b2
Caio Vitor <caiovitorlopes09@gmail.com>
2026-09-06 21:45:52 -0300

    Separe a renderização da fila

--- public/secretaria.js
+++ public/secretaria.js
@@ -37,17 +37,7 @@ async function fazerRequisicaoProtegida(url, opcoes = {}) {
   return resposta;
 }
 
-
-async function carregarFila() {
-  const resposta =
-    await fazerRequisicaoProtegida("/fila");
-
-  if (!resposta.ok) {
-    return;
-  }
-
-  const fila = await resposta.json();
-
+function renderizarFila(fila) {
   listaFila.innerHTML = "";
 
   const pessoasAguardando = fila.filter(
@@ -69,6 +59,19 @@ async function carregarFila() {
       </div>
     `;
   });
+}
+
+async function carregarFila() {
+  const resposta =
+    await fazerRequisicaoProtegida("/fila");
+
+  if (!resposta.ok) {
+    return;
+  }
+
+  const fila = await resposta.json();
+
+  renderizarFila(fila);
 }
 
 
